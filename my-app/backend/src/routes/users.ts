1063b9e65f6c5acff8f9620de30baac8c3f13f1a
Junmiann <Mimmixu@gmail.com>
2025-10-09 15:23:08 +0200

    Update registration

--- my-app/backend/src/routes/users.ts
+++ my-app/backend/src/routes/users.ts
@@ -1,14 +1,56 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import type { User } from "../types/user.js";
 import pool from "../db.js";
+import bcrypt from 'bcrypt';
+import type { User } from "../types/user.js";
 
 const router = Router();
 
-const users: User[] = [
-  { id: 1, firstName: "Anna", lastName: "Johnson", groupId: 3 },
-  { id: 2, firstName: "Bob", lastName: "McKnight", groupId: 19 },
-];
+router.post("/register", async (req: Request, res: Response) => {
+  const { groupId, firstName, lastName, email, password } = req.body as User;
+
+  const client = await pool.connect();
+
+  try {
+    const groupIdResult = await client.query("SELECT id FROM groups WHERE id = $1", [groupId]);
+    if (groupIdResult.rowCount === 0) {
+      return res.status(400).json({ error: "Group ID does not exist" });
+    }
+
+    const emailResult = await client.query("SELECT email FROM login_credentials WHERE email = $1", [email]);
+    if (emailResult.rowCount === 1) {
+      return res.status(409).json({ error: "The email is already registered" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await client.query("BEGIN");
+
+    const userResult = await client.query(
+        "INSERT INTO users (first_name, last_name, group_id) VALUES ($1, $2, $3)", 
+        [firstName, lastName, groupId]
+    );
+
+    const userId = userResult.rows[0].id;
+
+    await client.query(
+        "INSERT INTO login_credentials (user_id, email, password) VALUES ($1, $2, $3)", 
+        [userId, email, hashedPassword]
+    );
+
+    await client.query("COMMIT");
+
+    res.status(201).json({ message: "New user created", userId });
+
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+
+  } finally {
+    client.release();
+  }
+});
 
 /* TEST: will probably be changed onwards */
 router.get("/", async (req: Request, res: Response) => {
@@ -19,17 +61,6 @@ router.get("/", async (req: Request, res: Response) => {
     console.error("Error fetching users:", err);
     res.status(500).json({ error: "Could not fetch users" });
   }
-});
-
-router.post("/", (req: Request, res: Response) => {
-  const lastUser = users.at(-1);
-  const newId = lastUser ? lastUser.id + 1 : 1;
-
-  const { firstName, lastName, groupId } = req.body;
-
-  const newUser = { id: newId, firstName, lastName, groupId };
-  users.push(newUser);
-  res.json(newUser);
 });
 
 export default router;
--- my-app/backend/src/types/user.ts
+++ my-app/backend/src/types/user.ts
@@ -1,6 +1,7 @@
 export type User = {
-    id: number;
+    groupId: string;
     firstName: string;
     lastName: string;
-    groupId: number;
+    email: string;
+    password: string;
 };
