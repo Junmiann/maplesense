ea79801501823913337e09022d351e58144a07b0
Junmiann <Mimmixu@gmail.com>
2025-10-09 11:59:28 +0200

    Connect to database

---
+++ my-app/backend/src/db.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+});
+
+async function verifyConnection(): Promise<void> {
+    try {
+        const client = await pool.connect();
+        console.log("Connected to database");
+        client.release();
+    } catch (error) {
+        console.error("Failed to connect to database");
+    }
+}
+
+verifyConnection();
+
+export default pool;
