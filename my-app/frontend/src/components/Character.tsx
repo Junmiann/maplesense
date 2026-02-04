cf2c7d23e766820fdfb18963540fe28d60e1e8e5
Junmiann <Mimmixu@gmail.com>
2026-02-04 16:54:09 +0100

    Add CSS to character-page

--- my-app/frontend/src/components/Character.tsx
+++ my-app/frontend/src/components/Character.tsx
@@ -42,18 +42,25 @@ export default function Character() {
     }
 
     return (
-        <div>
-            <Link to="/classes">Back to classes</Link>
-
-            <h1>{character.name}</h1>
-            <p>Job: {character.job.join(", ")}</p>
-            <p>Origin: {character.origin}</p>
-            <p>Primary weapon: {character.primary_weapon.join(", ")}</p>
-            <p>Secondary weapon: {character.secondary_weapon.join(", ")}</p>
-            <p>Difficulty: {character.difficulty}</p>
-            <p>Mobility: {character.mobility}</p>
-            <p>Range: {character.range}</p>
-            <img src={character.image_url} alt={character.name} />
+        <div className="flex flex-col max-w-6xl mx-auto p-4">
+            <Link to="/classes" className="flex justify-start">Back to classes</Link>
+            <div className="flex justify-center items-center gap-20">
+                {/* Character information */}
+                <div>
+                    <h1 className="uppercase font-bold">{character.name}</h1>
+                    <p className="justify-center text-xl uppercase font-semibold mb-4">{character.job.join(", ")} | {character.origin}</p>
+                    <div className="flex flex-col w-lg max-w-lg mb-2 gap-4 text-start">
+                        <p className="bg-white/20 rounded-full p-2 pl-5"><span className="uppercase font-bold">Primary weapon:</span>{" "}{character.primary_weapon.join(", ")}</p>
+                        <p className="bg-white/20 rounded-full p-2 pl-5"><span className="uppercase font-bold">Secondary weapon:</span>{" "}{character.secondary_weapon.join(", ")}</p>
+                    </div>
+                    <div>
+                        <p>Difficulty: {character.difficulty}</p>
+                        <p>Mobility: {character.mobility}</p>
+                        <p>Range: {character.range}</p>
+                    </div>
+                </div>
+                <img src={character.image_url} alt={character.name} className="w-90 flex-shrink"/>
+            </div>
         </div>
     );
 }
