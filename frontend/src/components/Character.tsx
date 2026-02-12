da026276a3dca8865ac751615aaf0f23a928becf
Junmiann <Mimmixu@gmail.com>
2026-02-12 17:14:53 +0100

    Add statBar

--- frontend/src/components/Character.tsx
+++ frontend/src/components/Character.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, Link, Navigate } from "react-router-dom";
 import type { Class } from "../types";
+import StatBar from "../components/statBar";
 
 export default function Character() {
     const { id } = useParams();
@@ -57,24 +58,27 @@ export default function Character() {
                         <p className="p-2 pl-5 text-sm rounded-full bg-white/20"><span className="font-bold uppercase">Primary weapon:</span>{" "}{character.primary_weapon.join(", ")}</p>
                         <p className="p-2 pl-5 text-sm rounded-full bg-white/20"><span className="font-bold uppercase">Secondary weapon:</span>{" "}{character.secondary_weapon.join(", ")}</p>
                     </div>
-                    <div className="pl-5 mt-6">
+                    <div className="flex flex-col gap-3 pl-5 mt-6">
                         <div className="flex gap-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
                             </svg>
-                            <p><span className="font-bold uppercase">Difficulty:</span>{" "}{character.difficulty}</p>
+                            <p className="font-bold uppercase">Difficulty:</p>
+                            <StatBar value={character.difficulty} />
                         </div>
                         <div className="flex gap-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                             </svg>
-                            <p><span className="font-bold uppercase">Mobility:</span>{" "}{character.mobility}</p>
+                            <p className="font-bold uppercase">Mobility:</p>
+                            <StatBar value={character.mobility} />
                         </div>
                         <div className="flex gap-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
                             </svg>
-                            <p><span className="font-bold uppercase">Range:</span>{" "}{character.range}</p>
+                            <p className="font-bold uppercase">Range:</p>
+                            <StatBar value={character.range} />
                         </div>
                     </div>
                 </div>
---
+++ frontend/src/components/StatBar.tsx
@@ -0,0 +1,26 @@
+type StatBarProps = {
+  value: number;
+};
+
+export default function StatBar({ value }: StatBarProps) {
+  const safeValue = Math.max(0, Math.min(5, Math.round(value)));
+
+  return (
+    <div className="flex gap-1">
+      {Array.from({ length: 5 }).map((_, i) => {
+        const filled = i < safeValue;
+
+        return (
+          <div
+            key={i}
+            className={`h-6 w-8 rounded-tl-lg rounded-br-lg rounded-tr-md rounded-bl-md border transition-all
+              ${filled 
+                ? "bg-gradient-to-t from-[#8EB3BE] to-white"
+                : "border-white/40 bg-transparent"
+              }`}
+          />
+        );
+      })}
+    </div>
+  );
+}
