748b3f053182568c619f498301b59f0e856d0b5d
Junmiann <Mimmixu@gmail.com>
2026-09-03 13:54:59 +0200

    feat: added functions for admin authentication
    added functions:
    * getAdminByUsername()
    * checkPassword()
    
    updated:
    * authenticateAdmin()

--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,6 +1,51 @@
+import bcrypt from "bcrypt";
+
+import pool from "../db.js";
+import type { Admin } from "../types/admin.js";
+
 export async function authenticateAdmin (username: string, password: string) {
-    // TODO: Implement actual authentication logic
-    // and check against DB
+    try {
+        const admin = await getAdminByUsername(username);
+
+        if (!admin) {
+            return false;
+        }
+
+        const passwordIsValid = await checkPassword(admin, password);
+
+        return passwordIsValid;
     
-    return false;
+    } catch {
+        throw new Error("Login failed");
+    };
+};
+
+export async function checkPassword(admin: Admin, password: string) {
+    const passwordIsValid = await bcrypt.compare(
+        password,
+        admin.password_hash
+    );
+
+    return passwordIsValid;
+};
+
+export async function getAdminByUsername(username:string) {
+    try {
+        const adminExists = await pool.query(
+            `SELECT *
+            FROM admins 
+            WHERE username=$1`,
+            [username]
+        );
+
+        if (adminExists.rows.length > 0) {
+            const admin = adminExists.rows[0]
+            return admin;
+        }
+
+        return false;
+
+    } catch {
+        throw new Error("Can't find an admin with this username.");
+    };
 };
---
+++ backend/src/types/admin.ts
@@ -0,0 +1,5 @@
+export type Admin = {
+    id: number;
+    username: string;
+    password_hash: string;
+};
