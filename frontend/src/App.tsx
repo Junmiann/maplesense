3bc4c6581062d6ec5a79652582980b96baa32ecf
Junmiann <Mimmixu@gmail.com>
2026-09-21 15:49:29 +0200

    refactor: remove navbar from admin pages
    * added PublicLayout
    * moved Navbar to PublicLayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,27 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Nav.js";
 import Home from './components/Home.js';
 
+import PublicLayout from "./components/layouts/PublicLayout.js";
 import Classes from "../src/components/classes/Classes.js";
 import Character from "./components/character/Character.js";
 
 import AdminLogin from "./components/admin/AdminLogin.js";
+import AdminPasswordChange from "./components/admin/AdminPasswordChange.js";
 import ProtectedRoute from "./components/admin/ProtectedRoute.js";
 import AdminDashboard from "./components/admin/AdminDashboard.js";
-import AdminPasswordChange from "./components/admin/AdminPasswordChange.js";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/classes" element={<Classes />} />
-        <Route path="/classes/:id" element={<Character />} />
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/classes/:id" element={<Character />} />
+        </Route>
+
         <Route path="/admin/login" element={<AdminLogin />}/>
         <Route path="/admin/change-password" element={<AdminPasswordChange />}/>
-
         <Route 
           path="/admin/dashboard" 
           element={
